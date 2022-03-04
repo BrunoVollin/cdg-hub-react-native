d1db4ad79c23f2515dbfa9593fba2a92d616d517
bruno <bruno.3av@gmail.com>
2022-03-04 16:17:10 -0300

    init

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function addCounter() {
+    setCount(count + 1)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.counter}>{count}</Text>
+      <Button title="+ 1" onPress={addCounter} />
     </View>
   );
 }
@@ -13,8 +19,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-evenly", // espaçamento entre os componentes
+  },
+
+  counter: {
+    fontSize: 40,
   },
 });
